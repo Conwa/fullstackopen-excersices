918043f29a31ab8da4a02b9df4807167f8624740
Conwa <conradodelcarlo97@gmail.com>
2022-11-08 18:12:18 -0300

    trying to upload it to render.com

--- part3/guide/index.js
+++ part3/guide/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 let notes = [
   {
@@ -29,6 +30,7 @@ let notes = [
 ];
 
 /*METODOS*/
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (request, response) => {
