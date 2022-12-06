c5584b66b4925cc8489007effccd6f43c2cf2483
Conwa <conradodelcarlo97@gmail.com>
2022-12-06 16:27:07 -0300

    started to set testing env and routes

---
+++ part4/blog/tests/test_helper.js
@@ -0,0 +1,46 @@
+const initialBlogs = [
+  {
+    _id: "638df0fad62ee5fc1524610d",
+    title: "test",
+    author: "juan",
+    url: "sdfdsfs",
+    likes: 12312,
+    __v: 0,
+  },
+  {
+    _id: "638df56af31ebbb12e6c1206",
+    title: "test2",
+    author: "juan",
+    url: "sdfdsfs",
+    likes: 12312,
+    __v: 0,
+  },
+  {
+    _id: "638dfa39b379b0a9fb796105",
+    title: "test after formatting",
+    author: "juan",
+    url: "sdfdsfs",
+    likes: 12312,
+    __v: 0,
+  },
+  {
+    _id: "638e6370d7c39670322dd98e",
+    title: "test4",
+    author: "juan",
+    url: "sdfdsfs",
+    likes: 15100,
+    __v: 0,
+  },
+  {
+    _id: "638e6379d7c39670322dd990",
+    title: "test5",
+    author: "juan",
+    url: "sdfdsfs",
+    likes: 18000,
+    __v: 0,
+  },
+];
+
+module.exports = {
+  initialBlogs,
+};
