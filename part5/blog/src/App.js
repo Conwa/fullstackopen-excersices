73c435d813a180f6314e86840d6aae2e05a6902d
Conwa <conradodelcarlo97@gmail.com>
2022-12-28 19:25:33 -0300

    5.1 completed

--- part5/blog/src/App.js
+++ part5/blog/src/App.js
@@ -1,24 +1,81 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      console.log(user);
+      setPassword("");
+      setUsername("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loginView = () => (
+    <>
+      <h2>login to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  );
+
+  const loggedView = () => (
+    <>
+      {" "}
+      <h2>BLOGS</h2>
+      <>
+        {" "}
+        <h3>{user.name} logged in</h3>
+        <button
+          onClick={() => {
+            console.log("gola");
+          }}
+        >
+          log out
+        </button>
+      </>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+      ))}
+    </>
+  );
 
-export default App
+  return <div>{user === null ? loginView() : loggedView()}</div>;
+};
+
+export default App;
