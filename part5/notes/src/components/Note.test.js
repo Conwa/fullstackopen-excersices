afb2cf3f580e9dba6b64b41b105dac1c4b47ba76
Conwa <conradodelcarlo97@gmail.com>
2023-01-26 20:16:54 -0300

    stopped at "running test" title

---
+++ part5/notes/src/components/Note.test.js
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Note from "./Note";
+
+test("renders content", () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  render(<Note note={note} />);
+
+  const element = screen.getByText(
+    "Component testing is done with react-testing-library"
+  );
+  expect(element).toBeDefined();
+});
