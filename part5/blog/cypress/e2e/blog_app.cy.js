7642318db5064ec98c8ef05fc35669c79f454185
Conwa <conradodelcarlo97@gmail.com>
2023-02-22 18:20:00 -0300

    it cleans the database

--- part5/blog/cypress/e2e/blog_app.cy.js
+++ part5/blog/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,16 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:3000/");
+describe("Blog App", () => {
+  beforeEach(function () {
+    cy.visit("");
+    cy.request("POST", "http://localhost:3001/api/testing/reset");
+    // const user = {
+    //   name: "Matti Luukkainen",
+    //   username: "mluukkai",
+    //   password: "salainen",
+    // };
+    // cy.request("POST", "http://localhost:3001/api/users/", user);
+  });
+
+  it("shows login by default", function () {
+    cy.contains("login to application");
   });
 });
