579021ad48b81ede77404eda9f20acba0fccd220
Conwa <conradodelcarlo97@gmail.com>
2023-03-20 16:48:24 -0300

    redux extension

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from "react-dom/client";
 import { createStore } from "redux";
 import counterReducer from "./reducer";
 
-const store = createStore(counterReducer);
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
   return (
