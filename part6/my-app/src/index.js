f32c5648150a2eb22107bb4942eb107252a99358
Conwa <conradodelcarlo97@gmail.com>
2023-03-22 17:55:24 -0300

    set for exersice 6.9

--- part6/my-app/src/index.js
+++ part6/my-app/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
 import App from "./App";
-import noteReducer from "./reducers/noteReducer";
+import filterReducer, { filterChange } from "./reducers/filterReducer";
+import noteReducer, { createNote } from "./reducers/noteReducer";
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
 
 const store = createStore(
-  noteReducer,
+  reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
@@ -16,3 +22,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </Provider>
 );
+
+// store.subscribe(() => console.log(store.getState()));
+// store.dispatch(filterChange("IMPORTANT"));
+// store.dispatch(
+//   createNote("combineReducers forms one reducer from many simple reducers")
+// );
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <Provider store={store}>
+//     <div />
+//   </Provider>
+// );
