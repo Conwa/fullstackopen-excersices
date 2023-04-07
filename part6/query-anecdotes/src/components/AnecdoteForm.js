45da41a5a36c076a7404174b065042b0cb74080f
Conwa <conradodelcarlo97@gmail.com>
2023-04-07 19:39:50 -0300

    6.12 on the making

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,21 +1,23 @@
-const AnecdoteForm = () => {
+import { useQuery } from "react-query";
+import { create } from ;
 
+const AnecdoteForm = () => {
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    console.log("new anecdote");
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
