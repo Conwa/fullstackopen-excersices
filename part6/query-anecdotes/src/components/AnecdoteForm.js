942b8a93bc701d8d41f94f6a318d8a6fa413ed31
Conwa <conradodelcarlo97@gmail.com>
2023-04-08 10:07:46 -0300

    6.21 done

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,12 +1,22 @@
-import { useQuery } from "react-query";
-import { create } from ;
+import { useMutation, useQueryClient } from "react-query";
+import { createAnecdote } from "../requests";
 
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("anecdotes");
+    },
+  });
+
   const onCreate = (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
+
+    newAnecdoteMutation.mutate({ content, votes: 0 });
+
     event.target.anecdote.value = "";
-    console.log("new anecdote");
   };
 
   return (
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -4,5 +4,9 @@ const baseUrl = "http://localhost:3001/anecdotes";
 
 export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data);
 
-export const createAnecdote = (newNote) =>
-  axios.post(baseUrl, newNote).then((res) => res.data);
+export const createAnecdote = (newAnecdote) => {
+  axios
+    .post(baseUrl, newAnecdote)
+    .then((res) => res.data)
+    .catch((err) => console.log(err.response.data));
+};
