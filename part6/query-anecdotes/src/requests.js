a22e5c92fe00913f19351ba9d5e13361cbfd7052
Conwa <conradodelcarlo97@gmail.com>
2023-04-10 18:55:31 -0300

    now done 6.24

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -5,7 +5,7 @@ const baseUrl = "http://localhost:3001/anecdotes";
 export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data);
 
 export const createAnecdote = (content) =>
-  axios.post({ baseUrl }, content).then((res) => res.data);
+  axios.post(`${baseUrl}`, content).then((res) => res.data);
 
 export const voteAnecdote = (updatedAnecdote) => {
   const id = updatedAnecdote.id;
