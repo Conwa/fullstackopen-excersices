9ec14877c16be68186255820f60cad43d5ba452e
Conwa <conradodelcarlo97@gmail.com>
2023-05-11 16:42:29 -0300

    individual user view working

--- part7/blog-redux-front/src/components/User.js
+++ part7/blog-redux-front/src/components/User.js
@@ -1,6 +1,27 @@
 const User = ({ author }) => {
-  console.log(author);
-  return <h2>{author.name} </h2>;
+  if (author === undefined) {
+    return null;
+  }
+  return (
+    <>
+      <h2>{author.name} </h2>
+      <>
+        {author.blogs.length === 0 ? (
+          <p style={{ fontWeight: "bold" }}>User has no blogs of his own!</p>
+        ) : (
+          <div>
+            <p style={{ fontWeight: "bold" }}>Added Blogs:</p>
+            <ul>
+              {" "}
+              {author.blogs.map((el) => (
+                <li key={el.id}> {el.title} </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </>
+    </>
+  );
 };
 
 export default User;
