9824894f32f98f49e85b433be445fea4e9121a7c
Conwa <conradodelcarlo97@gmail.com>
2023-06-06 17:59:23 -0300

    test2

--- part7/blog-redux-front/src/services/authors.js
+++ part7/blog-redux-front/src/services/authors.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 //NEED TO CHANGE URL FOR WORKING ON VERCEL OR FOR WORKING ON PRIVATE COMPUTER
+// eslint-disable-next-line no-unused-vars
 const baseUrl = "/api/users";
 
 const urlForVercel = "https://blog-redux-back-two.vercel.app/api/users";
